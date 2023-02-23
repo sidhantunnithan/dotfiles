4378adfc89a302567e791eb3020318a8648ec8b1
contact@sidhantunnithan.com <contact@sidhantunnithan.com>
2023-02-24 01:01:29 +0530

    first commit

---
+++ nvim/lua/jindan/init.lua
@@ -0,0 +1,4 @@
+require("jindan.set")
+require("jindan.remap")
+
+vim.cmd.colorscheme "catppuccin"
---
+++ nvim/lua/jindan/remap.lua
@@ -0,0 +1,11 @@
+-- allows moving a block while in visual mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- keeps the cursor in the middle
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("n", "Q", "<nop>")
