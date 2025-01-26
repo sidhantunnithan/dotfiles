4231ee8dd99ff91696d3554ceb54b71305623bc9
Sidhant Unnithan <contact@sidhantunnithan.com>
2025-01-26 19:45:51 +0530

    updated tmux config

---
+++ tmux/setup.bash
@@ -0,0 +1,6 @@
+rm -rf ~/.config/tmux/plugins/tpm
+git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
+
+rm -rf ~/.config/tmux/plugins/catppuccin
+mkdir ~/.config/tmux/plugins/catppuccin
+git clone -b v2.1.2 https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux
