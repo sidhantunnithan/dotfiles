7a2894acf34432718dfe2a33259f4c9cd45d52c0
Sidhant Unnithan <contact@sidhantunnithan.com>
2025-11-22 20:20:20 +0530

    refactor lsp.lua

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,51 +1,37 @@
 return {
-	"neovim/nvim-lspconfig",
-	dependencies = {
-		"seblyng/roslyn.nvim",
-		"mason-org/mason-lspconfig.nvim",
-		"mason-org/mason.nvim",
-		{ "saghen/blink.cmp", build = "cargo build --release" },
+	{
+		"neovim/nvim-lspconfig",
 	},
-	config = function()
-		require("mason").setup({
-			registries = { "github:crashdummyy/mason-registry", "github:mason-org/mason-registry" },
-		})
-		require("mason-lspconfig").setup()
-		require("roslyn").setup()
-		require("blink.cmp").setup({
-			completion = {
-				documentation = { auto_show = true },
-			},
-			keymap = {
-				preset = "none",
-				["<C-j>"] = { "select_next", "fallback" },
-				["<C-k>"] = { "select_prev", "fallback" },
-				["<CR>"] = { "select_and_accept" },
-			},
-		})
+	{
+		"mason-org/mason.nvim",
+		config = function()
+			require("mason").setup({})
 
-		vim.diagnostic.config({
-			signs = {
-				numhl = {
-					[vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
-					[vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
-					[vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
-					[vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
-				},
-				text = {
-					[vim.diagnostic.severity.ERROR] = "X",
-					[vim.diagnostic.severity.HINT] = "?",
-					[vim.diagnostic.severity.INFO] = "I",
-					[vim.diagnostic.severity.WARN] = "!",
-				},
-			},
-			update_in_insert = true,
-			virtual_text = true,
-		})
+			vim.diagnostic.config({ virtual_text = true })
 
-		vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Jump to definition" })
-		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Jump to declaration" })
-		vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Jump to reference" })
-		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { desc = "Jump to implementation" })
-	end,
+			-- LSP Related Keymaps
+			vim.keymap.set("n", "gd", vim.lsp.buf.declaration, {})
+			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
+			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+		end,
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		dependencies = {
+			"neovim/nvim-lspconfig",
+			"mason-org/mason.nvim",
+		},
+		opts = {
+			ensure_installed = {
+				"lua_ls",
+			},
+		},
+	},
+	{
+		"stevearc/quicker.nvim",
+		ft = "qf",
+		---@module "quicker"
+		---@type quicker.SetupOptions
+		opts = {},
+	},
 }
