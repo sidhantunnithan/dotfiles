edd8ffb18343481026dc8d9ff91d3daf94ad8815
Sidhant Unnithan <contact@sidhantunnithan.com>
2026-03-08 16:15:41 +0530

    add leader-x keybindings to close buffer(s) with unsaved changes prompt

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -7,3 +7,33 @@ for i = 1, 9, 1 do
 	vim.keymap.set("n", "<leader>" .. i, i .. "gt", {})
 end
 vim.keymap.set("n", "<leader>0", ":tablast<cr>", {})
+
+vim.keymap.set("n", "<leader>x", function()
+  if vim.bo.modified then
+    local choice = vim.fn.confirm("Save changes before closing?", "&Yes\n&No\n&Cancel", 1)
+    if choice == 1 then
+      vim.cmd("w | bd")
+    elseif choice == 2 then
+      vim.cmd("bd!")
+    end
+  else
+    vim.cmd("bd")
+  end
+end, { desc = "Close buffer" })
+
+vim.keymap.set("n", "<leader>X", function()
+  local modified_bufs = vim.tbl_filter(function(buf)
+    return vim.api.nvim_buf_is_loaded(buf) and vim.bo[buf].modified
+  end, vim.api.nvim_list_bufs())
+
+  if #modified_bufs > 0 then
+    local choice = vim.fn.confirm("There are unsaved changes. Save all before closing?", "&Yes\n&No\n&Cancel", 1)
+    if choice == 1 then
+      vim.cmd("wa | %bd")
+    elseif choice == 2 then
+      vim.cmd("%bd!")
+    end
+  else
+    vim.cmd("%bd")
+  end
+end, { desc = "Close all buffers" })
